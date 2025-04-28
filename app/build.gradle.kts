86fb4939c1465d2cc626ed6032ab464a2d92b08e
CARACOLKNITS\carmas <carmas@caracolknits.com>
2025-04-27 23:35:43 -0600

    Version code & version name manually

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.itanddev.necsmobile"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
