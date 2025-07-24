64a706cc35470521e6a2ec15b167e75dfbd085e5
CARACOLKNITS\carmas <carmas@caracolknits.com>
2025-07-23 22:05:13 -0600

    Implementing auto updater

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.26"
+        versionName = "1.0.27"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
